b7a2742958fadf768a2ea8dea52b8f1f6cc87236
Quan_Nguyen <quan.nguyen.2629@gmail.com>
2021-07-14 16:58:46 +0700

    add main menu

---
+++ src/App/Component/MainHeader/index.tsx
@@ -0,0 +1,66 @@
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  Box,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import {
+  makeStyles,
+  Theme,
+  createStyles,
+} from "@material-ui/core/styles";
+import { DRAWER_WIDTH } from "../../Constant";
+
+interface MainHeaderProps {
+  handleDrawerToggle: () => void;
+}
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appBar: {
+      [theme.breakpoints.up("sm")]: {
+        width: `calc(100% - ${DRAWER_WIDTH}px)`,
+        marginLeft: DRAWER_WIDTH,
+      },
+      height: 100,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
+      },
+    },
+  })
+);
+
+const MainHeader = ({ handleDrawerToggle }: MainHeaderProps) => {
+  const classes = useStyles();
+  return (
+    <AppBar position="fixed" className={classes.appBar} color="inherit">
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          className={classes.menuButton}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          style={{ height: 100, marginLeft: 60 }}
+        >
+          <Typography variant="h6" noWrap>
+            HOME
+          </Typography>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default MainHeader;
---
+++ src/App/Component/MainMenu/index.tsx
@@ -0,0 +1,195 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Typography from "@material-ui/core/Typography";
+import {
+  makeStyles,
+  useTheme,
+  Theme,
+  createStyles,
+} from "@material-ui/core/styles";
+import { DRAWER_WIDTH } from "../../Constant";
+import MainHeader from "../MainHeader";
+import ELearningIcon from "../Svg/ElearningIcon";
+import { Box } from "@material-ui/core";
+import HomeIcon from "../Svg/HomeIcon";
+import PersonIcon from "../Svg/PersonIcon";
+import StockIcon from "../Svg/StockIcon";
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    drawer: {
+      [theme.breakpoints.up("sm")]: {
+        width: DRAWER_WIDTH,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      [theme.breakpoints.up("sm")]: {
+        width: `calc(100% - ${DRAWER_WIDTH}px)`,
+        marginLeft: DRAWER_WIDTH,
+      },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
+      },
+    },
+    // necessary for content to be below app bar
+    toolbar: {
+      height: 100,
+    },
+    drawerPaper: {
+      width: DRAWER_WIDTH,
+      backgroundColor: "#373B53",
+      borderRightWidth:0
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  })
+);
+
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+}
+
+function MainMenu(props: Props) {
+  const { window } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <Divider />
+      <Box display="flex" justifyContent="center" style={{ paddingTop: 29 }}>
+        <img src="logo.png" alt="Logo" />
+      </Box>
+      <List>
+        <ListItem button style={{backgroundColor:'white'}}>
+          <ListItemIcon
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <HomeIcon />
+          </ListItemIcon>
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <PersonIcon/>
+          </ListItemIcon>
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <StockIcon />
+          </ListItemIcon>
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <MainHeader handleDrawerToggle={handleDrawerToggle} />
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+        <ELearningIcon />
+      </main>
+    </div>
+  );
+}
+
+export default MainMenu;
